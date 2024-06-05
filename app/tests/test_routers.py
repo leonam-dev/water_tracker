24f1811b215c332aea28d91eb3350ca2b4148c78
Leonam Raone <leonam.raone@gmail.com>
2024-06-05 15:05:05 -0300

    test: add tests for water intake service and API

--- app/tests/test_routers.py
+++ app/tests/test_routers.py
@@ -1,4 +1,7 @@
+import uuid, pytz
+
 from http import HTTPStatus
+from datetime import datetime
 
 
 def test_create_user_route(client):
@@ -32,3 +35,44 @@ def test_create_user_route_invalid_data(client):
     assert 'weight' in data['message']
     assert 'Weight must be greater than zero.' in data['message']['weight']
 
+
+def test_add_intake_route(client, test_user):
+    response = client.post(f'/api/v1/users/{test_user.id}/intakes', json={'amount': 500})
+    assert response.status_code == HTTPStatus.CREATED
+    data = response.get_json()
+    assert data['amount'] == 500
+
+
+def test_add_intake_invalid_data(client, test_user):
+    # missing amount
+    response = client.post(f'/api/v1/users/{test_user.id}/intakes', json={'user_id': test_user.id})
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+    # invalid amount
+    response = client.post(f'/api/v1/users/{test_user.id}/intakes', json={'amount': -500})
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_get_intake_route(client, test_user):
+    date = datetime.now(pytz.timezone('America/Sao_Paulo')).date().strftime('%d-%m-%Y')
+    client.post(f'/api/v1/users/{test_user.id}/intakes', json={'amount': 500})
+    response = client.get(f'/api/v1/users/{test_user.id}/intakes?date={date}')
+    assert response.status_code == HTTPStatus.OK
+    data = response.get_json()
+    assert data['total_intake'] == 500
+
+
+def test_get_intake_user_not_found(client):
+    date = datetime.now(pytz.timezone('America/Sao_Paulo')).date().strftime('%d-%m-%Y')
+    response = client.get(f'/api/v1/users/{uuid.uuid4()}/intakes?date={date}')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_intakes_history_route(client, test_user):
+    client.post(f'/api/v1/users/{test_user.id}/intakes', json={'amount': 500})
+    client.post(f'/api/v1/users/{test_user.id}/intakes', json={'amount': 1000})
+    response = client.get(f'/api/v1/users/{test_user.id}/intakes-history')
+    assert response.status_code == HTTPStatus.OK
+    data = response.get_json()
+    assert len(data) == 1
+    assert data[0]['total_intake'] == 1500
--- app/tests/test_services.py
+++ app/tests/test_services.py
@@ -1,6 +1,8 @@
-import pytest
-from app.models import User
-from app.services import UserService
+import pytest, pytz
+
+from datetime import datetime
+from app.models import User, WaterIntake
+from app.services import UserService, WaterIntakeService
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -26,3 +28,34 @@ def test_get_user_not_found(app):
     with app.app_context():
         with pytest.raises(NoResultFound):
             UserService.get_user(123)
+
+
+def test_add_intake(app, test_user):
+    with app.app_context():
+        water_intake = WaterIntake(user_id=test_user.id, amount=500)
+        intake = WaterIntakeService.add_intake(water_intake)
+        assert intake.id is not None
+        assert intake.user_id == test_user.id
+        assert intake.amount == 500
+
+
+def test_get_intake_by_date(app, test_user):
+    with app.app_context():
+        water_intake = WaterIntake(user_id=test_user.id, amount=500)
+        WaterIntakeService.add_intake(water_intake)
+        water_intake.date = datetime.now(pytz.timezone('America/Sao_Paulo')).date()
+        intake = WaterIntakeService.get_intake_by_date(water_intake)
+        assert intake['total_intake'] == 500
+        assert intake['remaining'] == 1950
+
+
+def test_get_all_intake(app, test_user):
+    with app.app_context():
+        water_intake1 = WaterIntake(user_id=test_user.id, amount=500)
+        water_intake2 = WaterIntake(user_id=test_user.id, amount=1000)
+        WaterIntakeService.add_intake(water_intake1)
+        WaterIntakeService.add_intake(water_intake2)
+        intakes = WaterIntakeService.get_all_intake(user_id=test_user.id)
+        assert len(intakes) == 1
+        assert intakes[0]['total_intake'] == 1500
+        assert intakes[0]['remaining'] == 950
