9cc9af7a750b10d5cfb0386df85769ff0b7c2254
Leonam Raone <leonam.raone@gmail.com>
2024-06-05 12:08:52 -0300

    feat: add database models

---
+++ app/models.py
@@ -0,0 +1,30 @@
+import uuid, pytz
+
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(80), nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    daily_goal = db.Column(db.Float, nullable=False)
+
+    def __init__(self, name, weight):
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.weight = weight
+        self.daily_goal = weight * 35 if weight is not None else 0
+
+    intakes = db.rellationship('WaterIntake', backref='user', lazy=True)
+
+
+class WaterIntake(db.Model):
+    __tablename__ = 'water_intakes'
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Datetime, default=lambda: datetime.now(pytz.timezone('America/Sao_Paulo')), nullable=False)
